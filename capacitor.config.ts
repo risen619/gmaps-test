e4fea31b352f1d6f034ac501cbffb9eca46be531
Andrew Komar <andrew@leverage.it>
2023-03-30 01:00:55 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'google-maps-api-script-fail',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
